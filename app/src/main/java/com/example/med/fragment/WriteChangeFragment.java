cdf820f951a5b035ff69d6ff2cb215d24f6006b9
Blablasunny <91792426+Blablasunny@users.noreply.github.com>
2022-05-27 18:22:42 +0300

    0.1.3

--- app/src/main/java/com/example/med/fragment/WriteChangeFragment.java
+++ app/src/main/java/com/example/med/fragment/WriteChangeFragment.java
@@ -16,6 +16,9 @@ public class WriteChangeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_write_change, container, false);
+
+
+
         return view;
     }
 }
